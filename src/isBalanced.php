bfa80ce7606c91bf81130e3f346e790f3a67e0b3
Alex <alekseimell5644@gmail.com>
2019-03-13 13:12:07 +0300

    fix mistake CC

--- src/isBalanced.php
+++ src/isBalanced.php
@@ -6,20 +6,15 @@ function isBalanced($str)
     $balanceArray=['[]', '{}', '()'];
     $openArray=['[', '{', '('];
     $count=strlen($str);
+    $balanceStr = '';
 
     for($i = 0; $i < $count; $i++) {
         if (in_array($str[$i], $openArray)) {
             array_push($stackArray, $str[$i]);
         }else {
-            $balanceStr = array_pop($stackArray) . $str[$i];
-
-            if(in_array($balanceStr, $balanceArray)) {
-                $balanceStr = '';
-            }else {
-                return false;
-            }
+            $balanceStr = array_pop($stackArray) . $str[$i];    
+            return in_array($balanceStr, $balanceArray);
         }
     }
-
-    return empty($stackArray);
+        return empty($stackArray);
 }
