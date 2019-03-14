321c31adb265e8906aad7cd604b988400b2eebad
Alex <alekseimell5644@gmail.com>
2019-03-14 11:57:09 +0300

    travis linter edit

--- src/isBalanced.php
+++ src/isBalanced.php
@@ -8,11 +8,11 @@ function isBalanced($str)
     $count=strlen($str);
     $balanceStr = '';
 
-    for($i = 0; $i < $count; $i++) {
+    for ($i = 0; $i < $count; $i++) {
         if (in_array($str[$i], $openArray)) {
             array_push($stackArray, $str[$i]);
-        }else {
-            $balanceStr = array_pop($stackArray) . $str[$i];    
+        } else {
+            $balanceStr = array_pop($stackArray) . $str[$i];
             return in_array($balanceStr, $balanceArray);
         }
     }
